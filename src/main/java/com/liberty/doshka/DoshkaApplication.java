8c756f8b267ffe3a375b81baa82a3f908c755f2f
Dmytro Kovalskyi <dkovalskyi@playtika.com>
2017-07-31 10:46:19 +0300

    Initial commit

---
+++ src/main/java/com/liberty/doshka/DoshkaApplication.java
@@ -0,0 +1,12 @@
+package com.liberty.doshka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoshkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoshkaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/liberty/doshka/DoshkaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.liberty.doshka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DoshkaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
